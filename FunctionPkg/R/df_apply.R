7111ae7107abaa26851adf40d088ff0e8fa714c5
Haoping He <yh38@students.calvin.edu>
2022-03-18 10:52:16 -0400

    fixed more stuff

--- FunctionPkg/R/df_apply.R
+++ FunctionPkg/R/df_apply.R
@@ -1,16 +1,28 @@
 #' "Transform" a data frame
-#' @.data – a data frame
-#' @.f – a function
-#' @.condition – a function – the default should TRUE for numeric columns and false for other columns
-#' @.else – a function to apply when .condition() evaluates to FALSE
-#' @... – additional arguments that will be passed to .f()
+#' @param .data – a data frame
+#' @param .f – a function
+#' @param .condition – a function – the default should TRUE for numeric columns and false for other columns
+#' @param .else – a function to apply when .condition() evaluates to FALSE
+#' @param ... – additional arguments that will be passed to .f()
+#'
+#' @return a data frame with specific change made to it.
+#' @examples
 
+#' n <- 5L
+#' TestData <- dplyr::tibble( double = c(5.002, 3.221, 4.008, 5.112, 4.779),
+#'                 integer   = c(0, 10, 20, 30, 40),
+#'                 cap = LETTERS[1L:n],
+#'                 low = letters[1L:n])
+#'   )
+#' df_apply(test_data, round, toupper, digits = 1)
+#' TestData |>  df_apply(toupper, is_character)
+#' TestData |> df_apply(round, is.numeric, toupper, digits = 2),
 
 
 df_apply <- function(.data, .f, .condition = is.numeric, .else = identity, ...){
   .data |>
     lapply( function(.x) if(.condition(.x)) .f(.x, ...) else .else(.x)) %>%
-    as.data.frame()
+    as_tibble()
 }
 
 
--- FunctionPkg/tests/testthat/test-test_df_apply.R
+++ FunctionPkg/tests/testthat/test-test_df_apply.R
@@ -1,24 +1,57 @@
 
-n <- 7L
+n <- 5L
 TestData <-
-  tibble( double = rnorm(n, 100, 10),
-          x = 123400 / 10^(1L:n),
-          integer = (1L:n) * (1L:n),    # ^2 would return a double!!
-          character = LETTERS[1L:n],
-          factor = factor(letters[1L:n]),
-          logical = rep(c(TRUE, FALSE), length.out = n) )
-
+  dplyr::tibble( double = c(5.002, 3.221, 4.008, 5.112, 4.779),
+                 integer   = c(0, 10, 20, 30, 40),
+                 cap = LETTERS[1L:n],
+                 low = letters[1L:n])
 
 test_that(
-  "functions works",
+  "Every parts of df_apply function work.",
   {
     expect_equal(
-      test_data |> df_apply(round, is.numeric),
-      test_data |> dplyr::mutate(double = c(95, 99, 97, 89, 90, 95, 98))
-    )
-    expect_equal(
-      test_data |> df_apply(tolower, is.character),
-      test_data |> dplyr::mutate(character = tolower(LETTERS[1L:n]))
+      TestData |> df_apply(round, is.numeric, toupper, digits = 2),
+      TestData |> dplyr::mutate(
+        double    = c(5.00, 3.22, 4.01, 5.11, 4.78),
+        integer   = c(0, 10, 20, 30, 40),
+        low = toupper(letters[1L:n])
+      )
     )
   }
 )
+
+
+test_that(
+  "condition and execution function works perfect.",
+  {
+    expect_equal(
+      TestData |> df_apply(round, is.numeric, digits = 2),
+      TestData |> dplyr::mutate(
+        double    = c(5.00, 3.22, 4.01, 5.11, 4.78),
+        integer   = c(0, 10, 20, 30, 40),
+        low =  letters[1L:n])
+    )
+    expect_equal(
+      TestData |>  df_apply(toupper, is_character),
+      TestData |> dplyr::mutate(
+        double    = c(5.002, 3.221, 4.008, 5.112, 4.779),
+        integer   = c(0, 10, 20, 30, 40),
+        low =  toupper(letters[1L:n]))
+    )
+  }
+)
+
+
+test_that(
+  "else function is optional but it works if added.",
+  {
+    expect_equal(
+      TestData |> df_apply(round, is.numeric, digits = 2),
+      TestData |> dplyr::mutate(
+        double    = c(5.00, 3.22, 4.01, 5.11, 4.78),
+        integer   = c(0, 10, 20, 30, 40),
+        low =  letters[1L:n])
+      )
+  }
+)
+
